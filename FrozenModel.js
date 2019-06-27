966b7ecb1d3f265db4f58cb9fc4c0d21bb896681
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2019-06-28 00:47:43 +0100

    FrozenModel JS prep

---
+++ FrozenModel.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+import * as tf from '@tensorflow/tfjs';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+
+const MODEL_URL = 'model_directory/model.json';
+
+const model = await; loadGraphModel(MODEL_URL);
+const cat = document.getElementById('cat');
+model.execute(tf.fromPixels(cat));
+
+class FrozenModel {
+    FrozenModel(){
+    }
+}
