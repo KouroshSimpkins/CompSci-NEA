223a82a3800b71e21f56b8555804648ff5b94988
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2019-09-12 20:15:09 +0100

    Image Ripper
    cv2 tool for ripping data from an image, correcting an image to from a random side angle to be a decent image in preperation for scanning data on the page.

---
+++ Img_Extraction/Text_Ripper.py
@@ -0,0 +1,20 @@
+try:
+    from PIL import Image
+finally:
+    import Image
+import PIL.Image
+from pytesseract import image_to_string
+import pytesseract
+import cv2
+import wand
+
+imS = cv2.resize(warped, (1350, 1150))
+cv2.imshow("output", imS)
+cv2.imwrite('Output Image.PNG', imS)
+cv2.waitKey(0)
+
+pytesseract.pytesseract.tesseract_cmd = 'image'
+TESSDATA_PREFIX = 'image'
+output = pytesseract.image_to_string
+(PIL.Image.open('OutputImage.PNG').convert("RGB"), lang='eng')
+print(output)
