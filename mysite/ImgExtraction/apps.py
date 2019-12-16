338a269c7eeb7dc49f3d84bcc57e7288a5f7c3db
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2019-12-16 15:37:23 +0000

    Housekeeping
    Removal of a whole load of files that were just unused within the scope of the project. Still on local, but not clogging up the repo as they aren't necessary for the project.

--- mysite/ImgExtraction/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImgextractionConfig(AppConfig):
-    name = 'ImgExtraction'
--- test.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-
-with open('/Users/kouroshsimpkins/OneDrive - Harris Federation/Programming/NeuralNetworks/CompSci NEA/Table_Test_Images/test_1.jpg', Image()):
-    print("test")
