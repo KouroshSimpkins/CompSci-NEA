ccab9c9d4b81af9cefeb9f8001f977c331d0319f
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2020-02-13 13:55:18 +0000

    Update validators.py

--- django_project/users/validators.py
+++ django_project/users/validators.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 
 
 def ValidateSchoolEmail(value):
-    if not "@hfed.net" in value:
+    if "@hfed.net" not in value:
         raise ValidationError("A valid Harris email address must be entered in")
     else:
         return value
