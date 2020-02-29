18c1e7270188d77ccb4ca2f293234ddffdaf13df
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2020-02-29 22:31:26 +0000

    Removing Django and Flask
    Having both installed was a mess, not sure how to deal with it so starting with flask again so I can try and fix my database problem. Also moving from sqlite to mysql.

--- django_project/users/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def ValidateSchoolEmail(value):
-    if "@hfed.net" not in value:
-        raise ValidationError("A valid Harris email address must be entered in")
-    else:
-        return value
